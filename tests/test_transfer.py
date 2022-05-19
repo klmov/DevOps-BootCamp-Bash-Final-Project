03f90af9c7aa4f1cd00d897a93f0ce9dac369d28
Kanstantsin Klimov <kanstantsin_klimov@epam.com>
2022-05-19 19:39:10 +0300

    Added tests

--- tests/test_transfer.py
+++ tests/test_transfer.py
@@ -16,7 +16,7 @@ def download_shellcheck():
             member.name = os.path.basename(member.name) # remove the path by reset it
     tar.extract(member,'.') # extrac
     tar.close()
-    return "/bin/bash ./shellcheck"
+    return "/bin/sh ./shellcheck"
 
 binary_path = download_shellcheck()
 
